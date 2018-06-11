92e7fe9af9593f050cd846029c3b07757ef77ba3
Andrew Mantha <10056952@gemalto.com>
2018-06-11 10:04:19 -0400

    Remove the SpringBoot banner.

--- src/main/java/com/gemalto/chaos/ChaosEngine.java
+++ src/main/java/com/gemalto/chaos/ChaosEngine.java
@@ -2,6 +2,7 @@ package com.gemalto.chaos;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,8 +13,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ChaosEngine
 {
     private static final Logger log = LoggerFactory.getLogger(ChaosEngine.class);
+
+    private static SpringApplication app;
+
     public static void main( String[] args )
     {
-        ApplicationContext context = SpringApplication.run(ChaosEngine.class);
+        app = new SpringApplication(ChaosEngine.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        ApplicationContext context = app.run();
+
     }
 }
