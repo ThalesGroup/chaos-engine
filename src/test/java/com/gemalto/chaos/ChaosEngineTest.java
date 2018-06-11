9e57d289510fe279926abdfde97e222b9f060ea7
Andrew Mantha <10056952@gemalto.com>
2018-06-11 10:34:35 -0400

    Add initial empty unit test

---
+++ src/test/java/com/gemalto/chaos/ChaosEngineTest.java
@@ -0,0 +1,18 @@
+package com.gemalto.chaos;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple ChaosEngine.
+ */
+public class ChaosEngineTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
