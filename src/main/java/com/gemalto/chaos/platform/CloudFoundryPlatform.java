d9c033985bd367c6d66872d38bd60fa03f002001
Andrew Mantha <10056952@gemalto.com>
2018-06-13 16:28:05 -0400

    Move Cloud Foundry APIs into their own class.

--- src/main/java/com/gemalto/chaos/platform/CloudFoundryPlatform.java
+++ src/main/java/com/gemalto/chaos/platform/CloudFoundryPlatform.java
@@ -3,24 +3,12 @@ package com.gemalto.chaos.platform;
 import com.gemalto.chaos.ChaosException;
 import com.gemalto.chaos.container.CloudFoundryContainer;
 import com.gemalto.chaos.container.Container;
-import org.cloudfoundry.client.CloudFoundryClient;
-import org.cloudfoundry.doppler.DopplerClient;
 import org.cloudfoundry.operations.DefaultCloudFoundryOperations;
 import org.cloudfoundry.operations.applications.ApplicationSummary;
-import org.cloudfoundry.reactor.ConnectionContext;
-import org.cloudfoundry.reactor.DefaultConnectionContext;
-import org.cloudfoundry.reactor.TokenProvider;
-import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
-import org.cloudfoundry.reactor.doppler.ReactorDopplerClient;
-import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
-import org.cloudfoundry.reactor.uaa.ReactorUaaClient;
-import org.cloudfoundry.uaa.UaaClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
@@ -28,77 +16,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@ConditionalOnProperty({"cf_apihost", "cf_username", "cf_password", "cf_organization"})
+@ConditionalOnBean(DefaultCloudFoundryOperations.class)
 public class CloudFoundryPlatform implements Platform {
 
     private static final Logger log = LoggerFactory.getLogger(CloudFoundryPlatform.class);
 
     @Autowired
     private DefaultCloudFoundryOperations cloudFoundryOperations;
-
-    public CloudFoundryPlatform() {
-        log.info("Initialized!");
-    }
-
-    @Bean
-    DefaultConnectionContext defaultConnectionContext(@Value("${cf_apihost}") String apiHost,
-                                                      @Value("${cf_port:443}") int port) {
-        log.info("Creating a connection context");
-        return DefaultConnectionContext.builder()
-                .apiHost(apiHost)
-                .port(port)
-                .build();
-    }
-
-    @Bean
-    PasswordGrantTokenProvider tokenProvider(@Value("${cf_username}") String username,
-                                             @Value("${cf_password}") String password) {
-        log.info("Creating a token provider");
-        return PasswordGrantTokenProvider.builder()
-                .password(password)
-                .username(username)
-                .build();
-    }
-
-    @Bean
-    ReactorCloudFoundryClient cloudFoundryClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
-        return ReactorCloudFoundryClient.builder()
-                .connectionContext(connectionContext)
-                .tokenProvider(tokenProvider)
-                .build();
-    }
-
-    @Bean
-    ReactorDopplerClient dopplerClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
-        return ReactorDopplerClient.builder()
-                .connectionContext(connectionContext)
-                .tokenProvider(tokenProvider)
-                .build();
-    }
-
-    @Bean
-    ReactorUaaClient uaaClient(ConnectionContext connectionContext, TokenProvider tokenProvider) {
-        return ReactorUaaClient.builder()
-                .connectionContext(connectionContext)
-                .tokenProvider(tokenProvider)
-                .build();
-    }
-
-    @Bean
-    @ConditionalOnProperty({"cf_organization"})
-    DefaultCloudFoundryOperations cloudFoundryOperations(CloudFoundryClient cloudFoundryClient,
-                                                         DopplerClient dopplerClient,
-                                                         UaaClient uaaClient,
-                                                         @Value("${cf_organization}") String organization,
-                                                         @Value("${cf_space:default}") String space) {
-        return DefaultCloudFoundryOperations.builder()
-                .cloudFoundryClient(cloudFoundryClient)
-                .dopplerClient(dopplerClient)
-                .uaaClient(uaaClient)
-                .organization(organization)
-                .space(space)
-                .build();
-    }
 
     @Override
     public void degrade(Container container) {
