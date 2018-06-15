f4a75c0f56ab971194619c6ba62a8eb94296efcb
Andrew Mantha <10056952@gemalto.com>
2018-06-15 13:43:15 -0400

    Add a GlobalConfig package, and set the Task Scheduler to have 10 threads instead of default 1.

--- src/main/java/com/gemalto/chaos/services/CloudService.java
+++ src/main/java/com/gemalto/chaos/services/CloudService.java
@@ -1,4 +1,8 @@
 package com.gemalto.chaos.services;
 
+/**
+ * CloudService implentations should be used for instantiating Spring Beans
+ * to be used at the Platform level.
+ */
 public interface CloudService {
 }
