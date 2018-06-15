b8137e198ccc87878b5f833cfd26d2733692920c
Andrew Mantha <10056952@gemalto.com>
2018-06-15 10:42:42 -0400

    Implementing a Container Manager to maintain persistence of Container objects

--- src/main/java/com/gemalto/chaos/container/Container.java
+++ src/main/java/com/gemalto/chaos/container/Container.java
@@ -4,13 +4,20 @@ import com.gemalto.chaos.attack.Attack;
 import com.gemalto.chaos.attack.enums.AttackType;
 import com.gemalto.chaos.container.enums.ContainerHealth;
 import com.gemalto.chaos.fateengine.FateManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
 
 public abstract class Container {
+
+    protected final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private FateManager fateManager;
@@ -46,5 +53,40 @@ public abstract class Container {
 
     public void startAttack() {
         containerHealth = ContainerHealth.UNDER_ATTACK;
+    }
+
+
+    /**
+     * Uses all the fields in the container implementation (but not the Container parent class)
+     * to create a checksum of the container. This checksum should be immutable and can be used
+     * to recognize when building a roster if the container already exists, and can reference
+     * the same object.
+     *
+     * @return A checksum (format long) of the class based on the implementation specific fields
+     */
+    long getIdentity() {
+        ArrayList<Byte> byteArray = new ArrayList<>();
+        for (Field field : this.getClass().getDeclaredFields()) {
+            byte fieldByte = 0;
+            try {
+                fieldByte = field.getByte(this);
+            } catch (IllegalAccessException e) {
+                log.error("Failed to load bytes for field {}", field, e);
+            }
+
+            byteArray.add(fieldByte);
+
+        }
+        byte[] primitiveByteArray = new byte[byteArray.size()];
+        for (int i = 0; i < byteArray.size(); i++) {
+            primitiveByteArray[i] = byteArray.get(i);
+        }
+
+        Checksum checksum = new CRC32();
+        ((CRC32) checksum).update(primitiveByteArray);
+
+
+        return checksum.getValue();
+
     }
 }
---
+++ src/main/java/com/gemalto/chaos/container/ContainerManager.java
@@ -0,0 +1,47 @@
+package com.gemalto.chaos.container;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+@Component
+public class ContainerManager {
+
+    private HashMap<Class<? extends Container>, HashMap<Long, Container>> containerMap;
+
+    /**
+     * Returns all containers for a specific container class.
+     *
+     * @param containerType A class that extends the Container class.
+     * @return A Collection of Container objects from the given class.
+     */
+    public Collection<Container> getRoster(Class<Container> containerType) {
+        return getContainerTypeMap(containerType).values();
+    }
+
+    /**
+     * Given a container class, will return persistent HashMap of containers, identified by
+     * their serialization.
+     *
+     * @param containerClass A class which extends the Container class.
+     * @return A hashmap of containers, indexed by their serialization.
+     */
+    private HashMap<Long, Container> getContainerTypeMap(Class<? extends Container> containerClass) {
+        return containerMap.computeIfAbsent(containerClass, k -> new HashMap<>());
+    }
+
+    /**
+     * This will identify if a container has already been identified. If it has, it will
+     * return that container from the persistent Container Map. Otherwise, it will add the
+     * contained that was passed into the container map.
+     *
+     * @param container A discovered container.
+     * @return A persistent entry for that container that can hold a history.
+     */
+    public Container getOrCreatePersistentContainer(Container container) {
+        HashMap<Long, Container> containerTypeMap = getContainerTypeMap(container.getClass());
+        containerTypeMap.putIfAbsent(container.getIdentity(), container);
+        return containerTypeMap.get(container.getIdentity());
+    }
+}
