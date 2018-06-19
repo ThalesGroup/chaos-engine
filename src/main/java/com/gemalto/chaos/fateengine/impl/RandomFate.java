a51ad429163cfe2958c161483616219958eadcf1
Andrew Mantha <10056952@gemalto.com>
2018-06-19 08:39:21 -0400

    Improved the Random Fate unit test.

--- src/main/java/com/gemalto/chaos/fateengine/impl/RandomFate.java
+++ src/main/java/com/gemalto/chaos/fateengine/impl/RandomFate.java
@@ -1,6 +1,7 @@
 package com.gemalto.chaos.fateengine.impl;
 
 import com.gemalto.chaos.fateengine.FateEngine;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,14 +10,22 @@ import java.util.Random;
 @Component
 public class RandomFate extends FateEngine {
 
-    RandomFate(@Value("${probability:0.2}") float destructionProbability) {
-        this.destructionProbability = destructionProbability;
+    @Value("{probability:0.2}")
+    private float destructionProbability;
+    private Random random;
+
+    @Autowired
+    RandomFate() {
         minTimeToLive = 3;
         maxTimeToLive = 10;
         fateWeight = 10;
+        random = new Random();
     }
 
-    private float destructionProbability;
+    RandomFate(float destructionProbability, Random random) {
+        this.destructionProbability = destructionProbability;
+        this.random = random;
+    }
 
     private static boolean canDestroy(float destructionProbability, Random random) {
         return random.nextFloat() < destructionProbability;
@@ -24,7 +33,7 @@ public class RandomFate extends FateEngine {
 
     @Override
     public boolean canDestroy() {
-        return canDestroy(destructionProbability, new Random());
+        return canDestroy(destructionProbability, random);
     }
 
 }
--- src/test/java/com/gemalto/chaos/fateengine/impl/RandomFateTest.java
+++ src/test/java/com/gemalto/chaos/fateengine/impl/RandomFateTest.java
@@ -1,19 +1,43 @@
 package com.gemalto.chaos.fateengine.impl;
 
 import com.gemalto.chaos.fateengine.FateEngine;
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
+import java.util.Random;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
 public class RandomFateTest {
 
     private FateEngine fateEngine;
 
+    @Mock
+    private Random random;
+
     @Test
     public void canDestroy() {
-        fateEngine = new RandomFate(0);
-        boolean firstFate = fateEngine.canDestroy();
-        fateEngine = new RandomFate(1);
-        boolean secondFate = fateEngine.canDestroy();
-        Assert.assertNotEquals(firstFate, secondFate);
+        fateEngine = new RandomFate(0.2f, random);
+        Mockito.when(random.nextFloat())
+                .thenReturn(0.2f)
+                .thenReturn(0.1999f)
+                .thenReturn(0.0f)
+                .thenReturn(0.21f)
+                .thenReturn(1.0f);
+        assertFalse(fateEngine.canDestroy());
+
+        assertTrue(fateEngine.canDestroy());
+        assertTrue(fateEngine.canDestroy());
+
+
+        assertFalse(fateEngine.canDestroy());
+        assertFalse(fateEngine.canDestroy());
+
+
     }
 }
