0f207de541e78cf9a49465c30a71d10b0093cef4
Andrew Mantha <10056952@gemalto.com>
2018-06-21 16:53:27 -0400

    Added Unit Test for ChaosException exception.

---
+++ src/test/java/com/gemalto/chaos/ChaosExceptionTest.java
@@ -0,0 +1,42 @@
+package com.gemalto.chaos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ChaosExceptionTest {
+    private static final String MESSAGE = "This is why bad things happened";
+
+    @Test(expected = ChaosException.class)
+    public void emptyConstructor () {
+        throw new ChaosException();
+    }
+
+    @Test(expected = ChaosException.class)
+    public void stringConstructor () {
+        ChaosException chaosException = new ChaosException(MESSAGE);
+        assertEquals(MESSAGE, chaosException.getMessage());
+        throw chaosException;
+    }
+
+    @Test(expected = ChaosException.class)
+    public void stringThrowableConstructor () {
+        Exception causeException = new Exception();
+        ChaosException chaosException = new ChaosException(MESSAGE, causeException);
+        assertEquals(MESSAGE, chaosException.getMessage());
+        assertEquals(causeException, chaosException.getCause());
+        throw chaosException;
+    }
+
+    @Test(expected = ChaosException.class)
+    public void throwableConstructor () {
+        Exception causeException = new Exception();
+        ChaosException chaosException = new ChaosException(causeException);
+        assertEquals(chaosException.getCause().toString(), chaosException.getMessage());
+        assertEquals(causeException, chaosException.getCause());
+        throw chaosException;
+    }
+}
