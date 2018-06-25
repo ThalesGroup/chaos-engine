43f59f51e326071b8e289194e2f428f90294853d
Andrew Mantha <10056952@gemalto.com>
2018-06-25 11:21:40 -0400

    Adding more Unit Tests to the CloudFoundryPlatform class

--- src/test/java/com/gemalto/chaos/platform/impl/CloudFoundryPlatformTest.java
+++ src/test/java/com/gemalto/chaos/platform/impl/CloudFoundryPlatformTest.java
@@ -3,13 +3,17 @@ package com.gemalto.chaos.platform.impl;
 import com.gemalto.chaos.container.ContainerManager;
 import com.gemalto.chaos.platform.enums.ApiStatus;
 import org.cloudfoundry.operations.CloudFoundryOperations;
+import org.cloudfoundry.operations.applications.Applications;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CloudFoundryPlatformTest {
@@ -17,11 +21,35 @@ public class CloudFoundryPlatformTest {
     private CloudFoundryOperations cloudFoundryOperations;
     @Mock
     private ContainerManager containerManager;
+    @Mock
+    private Applications applications;
+    private CloudFoundryPlatform cfp;
+
+    @Before
+    public void setUp () {
+        cfp = new CloudFoundryPlatform(cloudFoundryOperations, containerManager);
+    }
 
     @Test
-    public void getApiStatus () {
-        CloudFoundryPlatform cfp = new CloudFoundryPlatform(cloudFoundryOperations, containerManager);
-        Mockito.when(cloudFoundryOperations.applications()).thenThrow(Mockito.mock(RuntimeException.class));
+    public void getApiStatusError () {
+        when(cloudFoundryOperations.applications()).thenThrow(mock(RuntimeException.class));
         assertEquals(ApiStatus.ERROR, cfp.getApiStatus());
+    }
+
+    @Test
+    public void getApiStatusSuccess () {
+        when(cloudFoundryOperations.applications()).thenReturn(applications);
+        assertEquals(ApiStatus.OK, cfp.getApiStatus());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void restartInstanceTest () {
+        Mono<Void> monoVoid = mock(Mono.class);
+        when(cloudFoundryOperations.applications()).thenReturn(applications);
+        when(applications.restartInstance(null)).thenAnswer((Answer<Mono<Void>>) invocationOnMock -> monoVoid);
+        cfp.restartInstance(null);
+        verify(applications, times(1)).restartInstance(null);
+        verify(monoVoid, times(1)).block();
     }
 }
