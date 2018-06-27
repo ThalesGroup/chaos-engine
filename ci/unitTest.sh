53212eeca2a53e6bece77b9153a165de595b6399
Andrew Mantha <andrew.mantha@gemalto.com>
2018-06-27 13:17:14 -0400

    Use Symlinks for accessing caches

--- ci/unitTest.sh
+++ ci/unitTest.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+M2_HOME="${HOME}/.m2"
+M2_CACHE="${ROOT_FOLDER}/maven"
+
+[[ -d "${M2_CACHE}" && ! -d "${M2_HOME}" ]] && ln -s "${M2_CACHE}" "${M2_HOME}"
+
 set -e -u
 cd chaos-engine/
 mvn -q test
