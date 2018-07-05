6a532040f70a3247b7a41d14bfff15863bdaca55
Andrew Mantha <andrew.mantha@gemalto.com>
2018-07-05 10:32:22 -0400

    Turn loggers back off in unit tests after running the Logging Controller test.

--- src/test/java/com/gemalto/chaos/logging/LoggingControllerTest.java
+++ src/test/java/com/gemalto/chaos/logging/LoggingControllerTest.java
@@ -1,17 +1,19 @@
 package com.gemalto.chaos.logging;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
 import com.gemalto.chaos.logging.enums.LoggingLevel;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.*;
 
 public class LoggingControllerTest {
     private static final String OTHER_CLASS = "org.springframework";
-    private static final Logger GEMALTO_LOGGER = LoggerFactory.getLogger("com.gemalto");
-    private static final Logger OTHER_LOGGER = LoggerFactory.getLogger(OTHER_CLASS);
+    private static final Logger GEMALTO_LOGGER = (Logger) LoggerFactory.getLogger("com.gemalto");
+    private static final Logger OTHER_LOGGER = (Logger) LoggerFactory.getLogger(OTHER_CLASS);
     private LoggingController loggingController;
 
     @Before
@@ -28,5 +30,11 @@ public class LoggingControllerTest {
         assertEquals("ERROR", loggingController.getLogLevel());
         assertTrue(GEMALTO_LOGGER.isErrorEnabled());
         assertFalse(GEMALTO_LOGGER.isWarnEnabled());
+    }
+
+    @After
+    public void tearDown () {
+        GEMALTO_LOGGER.setLevel(Level.OFF);
+        OTHER_LOGGER.setLevel(Level.OFF);
     }
 }
