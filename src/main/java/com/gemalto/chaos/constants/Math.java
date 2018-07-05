8a6240a6f3d26462426ecca3757e45719a245008
Andrew Mantha <andrew.mantha@gemalto.com>
2018-07-05 10:26:47 -0400

    Remove a useless exception in a static class private instantiation

--- src/main/java/com/gemalto/chaos/constants/Math.java
+++ src/main/java/com/gemalto/chaos/constants/Math.java
@@ -4,6 +4,5 @@ public class Math {
     public static final Double PHI = 1.61803398875;
 
     private Math () {
-        throw new AssertionError("Math constants class should never be instantiated.");
     }
 }
