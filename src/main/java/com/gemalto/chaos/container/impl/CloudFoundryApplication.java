9526c6fd2406087fc4b294aca896a919778cc1a6
Lubor PETR <lubor.petr@gemalto.com>
2018-08-07 14:48:12 +0200

    SCT-4909 Fixing logging of PCF application container

--- src/main/java/com/gemalto/chaos/container/impl/CloudFoundryApplication.java
+++ src/main/java/com/gemalto/chaos/container/impl/CloudFoundryApplication.java
@@ -16,9 +16,9 @@ import java.util.Random;
 import java.util.concurrent.Callable;
 
 public class CloudFoundryApplication extends Container {
-    private static final Integer MAX_INSTANCES = 5;
-    private static final Integer MIN_INSTANCES = 0;
-    private static final Logger log = LoggerFactory.getLogger(CloudFoundryApplication.class);
+    private transient static final Integer MAX_INSTANCES = 5;
+    private transient static final Integer MIN_INSTANCES = 0;
+    private transient static final Logger log = LoggerFactory.getLogger(CloudFoundryApplication.class);
     private transient String name;
     private Integer originalContainerInstances;
     private Integer actualContainerInstances;
