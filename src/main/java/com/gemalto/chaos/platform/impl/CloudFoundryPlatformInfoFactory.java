039ef9e0d328ad4cfdb49aa193cde198e0c884a0
Andrew Mantha <andrew.mantha@gemalto.com>
2018-08-22 16:20:29 -0400

    Fix CloudFoundryPlatformInfoFactory to use Abstract class reference instead of final class to allow for Vault Autowiring support.

--- src/main/java/com/gemalto/chaos/platform/impl/CloudFoundryPlatformInfoFactory.java
+++ src/main/java/com/gemalto/chaos/platform/impl/CloudFoundryPlatformInfoFactory.java
@@ -1,9 +1,9 @@
 package com.gemalto.chaos.platform.impl;
 
+import org.cloudfoundry.client.CloudFoundryClient;
 import org.cloudfoundry.client.v2.info.GetInfoRequest;
 import org.cloudfoundry.client.v2.info.GetInfoResponse;
 import org.cloudfoundry.operations.CloudFoundryOperations;
-import org.cloudfoundry.reactor.client.ReactorCloudFoundryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +17,11 @@ import reactor.core.publisher.Mono;
 public class CloudFoundryPlatformInfoFactory {
     private static final Logger log = LoggerFactory.getLogger(CloudFoundryPlatformInfoFactory.class);
     @Autowired
-    private ReactorCloudFoundryClient cloudFoundryClient;
+    private CloudFoundryClient cloudFoundryClient;
     @Autowired
     private CloudFoundryOperations cloudFoundryOperations;
 
-    CloudFoundryPlatformInfoFactory (ReactorCloudFoundryClient cloudFoundryClient, CloudFoundryOperations cloudFoundryOperations) {
+    CloudFoundryPlatformInfoFactory (CloudFoundryClient cloudFoundryClient, CloudFoundryOperations cloudFoundryOperations) {
         this.cloudFoundryClient = cloudFoundryClient;
         this.cloudFoundryOperations = cloudFoundryOperations;
     }
