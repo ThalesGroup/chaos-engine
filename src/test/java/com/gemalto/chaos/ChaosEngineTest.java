f8e5479dad44cebfc11070f5d47304775f49e731
Andrew Mantha <andrew.mantha@gemalto.com>
2018-08-22 10:18:47 -0400

    Fix an issue in Unit Test where Port was not castable to Integer.

--- src/test/java/com/gemalto/chaos/ChaosEngineTest.java
+++ src/test/java/com/gemalto/chaos/ChaosEngineTest.java
@@ -18,7 +18,7 @@ public class ChaosEngineTest {
         systemPropertiesMap = new HashSet<>();
         HashMap<String, String> cloudFoundry = new HashMap<>();
         cloudFoundry.put("cf.apihost", "localhost");
-        cloudFoundry.put("cf.port", "port");
+        cloudFoundry.put("cf.port", "443");
         cloudFoundry.put("cf.username", "username");
         cloudFoundry.put("cf.password", "password");
         cloudFoundry.put("cf.organization", "organization");
