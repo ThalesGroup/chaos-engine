b4530d1e54087fa020b48df26c44bab05dc811b3
Lubor PETR <lubor.petr@gemalto.com>
2018-08-22 11:07:39 +0200

    SCT-5361 updating attack constants

--- src/main/java/com/gemalto/chaos/constants/AttackConstants.java
+++ src/main/java/com/gemalto/chaos/constants/AttackConstants.java
@@ -2,6 +2,7 @@ package com.gemalto.chaos.constants;
 
 public class AttackConstants {
     public static final int DEFAULT_ATTACK_DURATION_MINUTES = 5;
+    public static final int DEFAULT_TIME_BEFORE_FINALIZATION_SECONDS = 30;
 
     private AttackConstants () {
     }
