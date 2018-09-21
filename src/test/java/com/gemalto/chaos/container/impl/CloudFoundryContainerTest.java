17d02bd92cb3fdbd51ce77c96b282cea7c2d2b54
Lubor PETR <lubor.petr@gemalto.com>
2018-09-21 15:58:07 +0200

    Extending CF Container unit tests

--- src/test/java/com/gemalto/chaos/container/impl/CloudFoundryContainerTest.java
+++ src/test/java/com/gemalto/chaos/container/impl/CloudFoundryContainerTest.java
@@ -2,7 +2,11 @@ package com.gemalto.chaos.container.impl;
 
 import com.gemalto.chaos.attack.Attack;
 import com.gemalto.chaos.attack.enums.AttackType;
+import com.gemalto.chaos.container.enums.ContainerHealth;
 import com.gemalto.chaos.platform.impl.CloudFoundryContainerPlatform;
+import com.gemalto.chaos.ssh.ShellSessionCapability;
+import com.gemalto.chaos.ssh.enums.ShellCapabilityType;
+import com.gemalto.chaos.ssh.enums.ShellSessionCapabilityOption;
 import com.gemalto.chaos.ssh.impl.attacks.ForkBomb;
 import com.gemalto.chaos.ssh.impl.attacks.RandomProcessTermination;
 import org.cloudfoundry.operations.applications.RestartApplicationInstanceRequest;
@@ -15,14 +19,14 @@ import org.mockito.Spy;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.UUID;
 import java.util.zip.CRC32;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 public class CloudFoundryContainerTest {
@@ -55,30 +59,33 @@ public class CloudFoundryContainerTest {
     }
 
     @Test
-    public void restartContainer () {
+    public void restartContainer () throws Exception {
         cloudFoundryContainer.restartContainer(attack);
         verify(attack, times(1)).setCheckContainerHealth(ArgumentMatchers.any());
         verify(attack, times(1)).setSelfHealingMethod(ArgumentMatchers.any());
         Mockito.verify(cloudFoundryContainerPlatform, times(1))
                .restartInstance(any(RestartApplicationInstanceRequest.class));
+        attack.getSelfHealingMethod().call();
     }
 
     @Test
-    public void forkBomb () {
+    public void forkBomb () throws Exception {
         cloudFoundryContainer.forkBomb(attack);
         verify(attack, times(1)).setCheckContainerHealth(ArgumentMatchers.any());
         verify(attack, times(1)).setSelfHealingMethod(ArgumentMatchers.any());
         Mockito.verify(cloudFoundryContainerPlatform, times(1))
                .sshAttack(any(ForkBomb.class), any(CloudFoundryContainer.class));
+        attack.getSelfHealingMethod().call();
     }
 
     @Test
-    public void terminateProcess () {
+    public void terminateProcess () throws Exception {
         cloudFoundryContainer.terminateProcess(attack);
         verify(attack, times(1)).setCheckContainerHealth(ArgumentMatchers.any());
         verify(attack, times(1)).setSelfHealingMethod(ArgumentMatchers.any());
         Mockito.verify(cloudFoundryContainerPlatform, times(1))
                .sshAttack(any(RandomProcessTermination.class), any(CloudFoundryContainer.class));
+        attack.getSelfHealingMethod().call();
     }
 
     @Test
@@ -86,5 +93,39 @@ public class CloudFoundryContainerTest {
         Attack attack = cloudFoundryContainer.createAttack(AttackType.STATE);
         assertEquals(cloudFoundryContainer, attack.getContainer());
         assertEquals(AttackType.STATE, attack.getAttackType());
+    }
+
+    @Test
+    public void getSimpleName () {
+        String EXPECTED_NAME = String.format("%s - (%s)", cloudFoundryContainer.getName(), cloudFoundryContainer.getInstance());
+        assertEquals(EXPECTED_NAME, cloudFoundryContainer.getSimpleName());
+    }
+
+    @Test
+    public void detectedCapabilities () {
+        ShellSessionCapability capabilityBash = new ShellSessionCapability(ShellCapabilityType.SHELL).addCapabilityOption(ShellSessionCapabilityOption.BASH);
+        ShellSessionCapability capabilityBinary = new ShellSessionCapability(ShellCapabilityType.BINARY).addCapabilityOption(ShellSessionCapabilityOption.GREP)
+                                                                                                        .addCapabilityOption(ShellSessionCapabilityOption.SORT);
+        ArrayList<ShellSessionCapability> capabilities = new ArrayList<>();
+        capabilities.add(capabilityBash);
+        capabilities.add(capabilityBinary);
+        cloudFoundryContainer.setDetectedCapabilities(capabilities);
+        assertEquals(capabilities, cloudFoundryContainer.getDetectedCapabilities());
+    }
+
+    @Test
+    public void getApplicationID () {
+        assertEquals(applicationId, cloudFoundryContainer.getApplicationId());
+    }
+
+    @Test
+    public void getPlatform () {
+        assertEquals(cloudFoundryContainerPlatform, cloudFoundryContainer.getPlatform());
+    }
+
+    @Test
+    public void updateContainerHealthImpl () {
+        doReturn(ContainerHealth.NORMAL).when(cloudFoundryContainerPlatform).checkHealth(applicationId, instance);
+        assertEquals(ContainerHealth.NORMAL, cloudFoundryContainer.updateContainerHealthImpl(AttackType.STATE));
     }
 }
