a1a361fa7401844f1a66a4ec5c634a6d0706e230
Andrew Mantha <andrew.mantha@gemalto.com>
2018-10-18 15:41:39 -0400

    Make use of Collections.synchronizedCollections rather than synchronize each block.

--- src/main/java/com/gemalto/chaos/container/ContainerManager.java
+++ src/main/java/com/gemalto/chaos/container/ContainerManager.java
@@ -3,28 +3,25 @@ package com.gemalto.chaos.container;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Optional;
 
 @Component
 public class ContainerManager {
-    private final Collection<Container> containers = new HashSet<>();
+    private final Collection<Container> containers = Collections.synchronizedCollection(new HashSet<>());
 
     public <T extends Container> T getMatchingContainer (Class<T> clazz, String uniqueIdentifier) {
-        synchronized (containers) {
-            Optional<T> match = containers.stream()
-                                          .filter(clazz::isInstance)
-                                          .map(clazz::cast)
-                                          .filter(container -> container.compareUniqueIdentifier(uniqueIdentifier))
-                                          .findFirst();
+        Optional<T> match = containers.stream()
+                                      .filter(clazz::isInstance)
+                                      .map(clazz::cast)
+                                      .filter(container -> container.compareUniqueIdentifier(uniqueIdentifier))
+                                      .findFirst();
             return match.orElse(null);
-        }
     }
 
     public void offer (Container container) {
-        synchronized (containers) {
-            containers.add(container);
-        }
+        containers.add(container);
     }
 
 }
