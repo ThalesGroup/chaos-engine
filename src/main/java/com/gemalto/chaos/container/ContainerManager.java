4a1c502d5ab584e42f1005f2c9ad8209e84aaafa
Andrew Mantha <andrew.mantha@gemalto.com>
2018-10-19 13:13:09 -0400

    Add JavaDocs to ContainerManager methods.

--- src/main/java/com/gemalto/chaos/container/ContainerManager.java
+++ src/main/java/com/gemalto/chaos/container/ContainerManager.java
@@ -10,6 +10,14 @@ import java.util.Optional;
 public class ContainerManager {
     private final Collection<Container> containers = new HashSet<>();
 
+    /**
+     * Returns an existing container of the specified class with the specified unique identifier (as validated by the
+     * Container's compareUniqueIdentifier method).
+     *
+     * @param clazz            A class that extends the com.gemalto.chaos.container.Container class.
+     * @param uniqueIdentifier The expected unique identifier for the container, as evaluated by Container::compareUniqueIdentifier
+     * @return Either an existing Container object, or null if no container exists.
+     */
     public <T extends Container> T getMatchingContainer (Class<T> clazz, String uniqueIdentifier) {
         Optional<T> match;
         synchronized (containers) {
@@ -22,6 +30,10 @@ public class ContainerManager {
         return match.orElse(null);
     }
 
+    /**
+     * Adds a container to the cache used by getMatchingContainer.
+     * @param container A Container object.
+     */
     public void offer (Container container) {
         synchronized (containers) {
             containers.add(container);
