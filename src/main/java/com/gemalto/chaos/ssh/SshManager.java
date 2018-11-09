49e9adb6ecb24247b24b0fefa191097368869445
Lubor PETR <lubor.petr@gemalto.com>
2018-11-09 11:47:52 +0100

    SCT-6514 - Busy waiting removed from SShManager, added SCP upload method

--- src/main/java/com/gemalto/chaos/ssh/SshManager.java
+++ src/main/java/com/gemalto/chaos/ssh/SshManager.java
@@ -1,16 +1,16 @@
 package com.gemalto.chaos.ssh;
 
 import net.schmizz.sshj.SSHClient;
-import net.schmizz.sshj.common.SSHException;
-import net.schmizz.sshj.connection.ConnectionException;
+import net.schmizz.sshj.common.IOUtils;
 import net.schmizz.sshj.connection.channel.direct.Session;
 import net.schmizz.sshj.connection.channel.direct.Session.Command;
-import net.schmizz.sshj.transport.TransportException;
 import net.schmizz.sshj.transport.verification.PromiscuousVerifier;
+import net.schmizz.sshj.xfer.FileSystemFile;
 import org.bouncycastle.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -31,70 +31,60 @@ public class SshManager {
     void setSshClient (SSHClient sshClient) {
         this.sshClient = sshClient;
     }
-    public boolean connect (String userName, String password) throws IOException{
-            log.debug("Connecting to host {}", hostname);
-            sshClient.connect(hostname, Integer.valueOf(port));
-            sshClient.authPassword(userName, password);
-            if (sshClient.isConnected() && sshClient.isAuthenticated()) {
-                log.debug("Connection to host {} succeeded.", hostname);
-                return true;
-            } else {
-                log.error("SSH Authentication failed.");
-            }
+
+    public void uploadFile(File file,String destinationPath) throws IOException {
+        sshClient.newSCPFileTransfer().upload(new FileSystemFile(file), destinationPath);
+    }
+
+    public boolean connect (String userName, String password) throws IOException {
+        log.debug("Connecting to host {}", hostname);
+        sshClient.connect(hostname, Integer.valueOf(port));
+        sshClient.authPassword(userName, password);
+        if (sshClient.isConnected() && sshClient.isAuthenticated()) {
+            log.debug("Connection to host {} succeeded.", hostname);
+            return true;
+        } else {
+            log.error("SSH Authentication failed.");
+        }
         return false;
     }
 
-    public void executeCommandInInteractiveShell (String command, String shellName, int maxSessionDuration) throws IOException{
-            Session session = sshClient.startSession();
-            session.allocateDefaultPTY();
-            log.debug("Going to execute command {} in interactive shell session {}", command, shellName);
-            Session.Shell shell = session.startShell();
-            shell.setAutoExpand(true);
-            OutputStream outputStream = shell.getOutputStream();
-            //Interactive command must be ended by new line
-            outputStream.write(Strings.toUTF8ByteArray(command + "\n"));
-            outputStream.flush();
-            InputStream stream = shell.getInputStream();
-            //Interactive session must be ended by exit command or sshClient connection drop
-            //ttl is there for security reasons when the library don't recognize disconnected channel
-            //can happen when aggressive experiments are performed
-            int ttl = 0;
-            while (sshClient.isConnected() && !shell.isEOF() && shell.isOpen() && !session.isEOF() && ttl < maxSessionDuration) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    log.error("Interactive SSH session watch dog has been interrupted.");
-                    break;
-                }
-                log.debug("Interactive SSH session {} is still active, TTL: {}", shellName, (maxSessionDuration - ttl));
-                ttl++;
-            }
-            log.debug("Interactive SSH session {} has ended. Closing the shell", shellName);
-            if (session.isOpen()) {
-                session.close();
-            }
-            if (shell.isOpen()) {
-                shell.close();
-            }
-            log.debug("Interactive shell session ended.");
+    public void executeCommandInShell (String command, String shellName, int maxSessionDuration) throws IOException {
+        Session session = sshClient.startSession();
+        session.allocateDefaultPTY();
+        log.debug("Going to execute command {} in interactive shell session {}", command, shellName);
+        Session.Shell shell = session.startShell();
+        shell.setAutoExpand(true);
+        OutputStream outputStream = shell.getOutputStream();
+        //Interactive command must be ended by new line
+        outputStream.write(Strings.toUTF8ByteArray(command + "\n"));
+        outputStream.flush();
+
+        log.debug("Interactive SSH session {} has ended. Closing the shell", shellName);
+        if (shell.isOpen()) {
+            shell.close();
+        }
+        if (session.isOpen()) {
+            session.close();
+        }
+
     }
 
     public SshCommandResult executeCommand (String command) throws IOException {
-            Session session = sshClient.startSession();
-            session.allocateDefaultPTY();
-            log.debug("Going to execute command: {}", command);
-            Command cmd = session.exec(command);
-            cmd.join(120, TimeUnit.SECONDS);
-            SshCommandResult result = new SshCommandResult(cmd);
-            session.close();
-            log.debug("Command execution finished with exit code: {}", cmd.getExitStatus());
-            return result;
+        Session session = sshClient.startSession();
+        session.allocateDefaultPTY();
+        log.debug("Going to execute command: {}", command);
+        Command cmd = session.exec(command);
+        cmd.join(120, TimeUnit.SECONDS);
+        SshCommandResult result = new SshCommandResult(cmd);
+        session.close();
+        log.debug("Command execution finished with exit code: {}", cmd.getExitStatus());
+        return result;
     }
 
-    public void disconnect () throws  IOException{
-            if (sshClient.isConnected()) {
-                sshClient.disconnect();
-            }
+    public void disconnect () throws IOException {
+        if (sshClient.isConnected()) {
+            sshClient.disconnect();
+        }
     }
 }
