10703e31685256069f1aa8d72d89f103b6fff181
Lubor PETR <lubor.petr@gemalto.com>
2018-11-20 15:59:55 +0100

    Increasing DEFAULT_SELF_HEALING_INTERVAL_MINUTES from 2 to 5 minutes

--- src/main/java/com/gemalto/chaos/constants/ExperimentConstants.java
+++ src/main/java/com/gemalto/chaos/constants/ExperimentConstants.java
@@ -3,7 +3,7 @@ package com.gemalto.chaos.constants;
 public class ExperimentConstants {
     public static final int DEFAULT_EXPERIMENT_DURATION_MINUTES = 5;
     public static final int DEFAULT_TIME_BEFORE_FINALIZATION_SECONDS = 30;
-    public static final int DEFAULT_SELF_HEALING_INTERVAL_MINUTES = 2;
+    public static final int DEFAULT_SELF_HEALING_INTERVAL_MINUTES = 5;
     public static final int DEFAULT_MAXIMUM_SELF_HEALING_RETRIES = 10;
     public static final String CANNOT_RUN_SELF_HEALING_AGAIN_YET = "Cannot run self healing again yet";
     public static final String SYSTEM_IS_PAUSED_AND_UNABLE_TO_RUN_SELF_HEALING = "System is paused and unable to run self healing";
