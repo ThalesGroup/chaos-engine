5f9bd1f9a9c634ee20aaea75dc154151d3a24197
Andrew Mantha <andrew.mantha@gemalto.com>
2018-12-06 12:48:10 -0500

    Clean up how tags are prefixed with branch names.

--- ci/build/tag-build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo ${BRANCH}-$(cat git-chaos-engine/.git/short_ref) > version/tags.txt
-
-if [ $BRANCH = "master" ] ; then
-  echo latest >> version/tags.txt
-else
-  echo ${BRANCH}-latest >> version/tags.txt
-fi
