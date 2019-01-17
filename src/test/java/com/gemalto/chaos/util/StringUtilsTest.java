537883712a47ff398328e27046d4fa8263fe7440
Andrew Mantha <andrew.mantha@gemalto.com>
2019-01-17 13:28:20 -0500

    Add a utility to convert a camelCaps string to readable format

--- src/test/java/com/gemalto/chaos/util/StringUtilsTest.java
+++ src/test/java/com/gemalto/chaos/util/StringUtilsTest.java
@@ -3,6 +3,7 @@ package com.gemalto.chaos.util;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class StringUtilsTest {
     @Test
@@ -10,5 +11,12 @@ public class StringUtilsTest {
         assertEquals("abc", StringUtils.addQuotesIfNecessary("abc"));
         assertEquals("\"ab cd\"", StringUtils.addQuotesIfNecessary("ab cd"));
         assertEquals("\"ab cd\"", StringUtils.addQuotesIfNecessary("\"ab cd\""));
+    }
+
+    @Test
+    public void camelCaseConverter () {
+        assertEquals("This Is How It Is", StringUtils.convertCamelCaseToSentence("thisIsHowItIs"));
+        assertEquals("Many Many Test Cases", StringUtils.convertCamelCaseToSentence("manyManyTestCases"));
+        assertNotEquals("Not Camel case", StringUtils.convertCamelCaseToSentence("notCamcelcase"));
     }
 }
