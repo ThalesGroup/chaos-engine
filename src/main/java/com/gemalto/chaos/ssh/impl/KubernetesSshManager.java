6d993ca04c0294eaccf17921888a1d5bcb9647d3
Eric Muellenbach <emuellenbach@gmx.de>
2019-02-05 14:25:24 +0100

    SCT-8409 Fix failing unit tests in Maven

--- src/main/java/com/gemalto/chaos/ssh/impl/KubernetesSshManager.java
+++ src/main/java/com/gemalto/chaos/ssh/impl/KubernetesSshManager.java
@@ -3,9 +3,7 @@ package com.gemalto.chaos.ssh.impl;
 import com.gemalto.chaos.ssh.SshManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class KubernetesSshManager extends SshManager {
     private static final Logger log = LoggerFactory.getLogger(KubernetesSshManager.class);
     private String podname, namespace;
