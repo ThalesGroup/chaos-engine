b9fb984fd1abe7c528f4c1b1b1a7015aa7fa8b7e
Andrew Mantha <andrew.mantha@gemalto.com>
2019-03-27 15:53:06 -0400

    Mark the Generic Exception-from-Throwable as Deprecated

--- src/main/java/com/gemalto/chaos/exception/ChaosException.java
+++ src/main/java/com/gemalto/chaos/exception/ChaosException.java
@@ -46,18 +46,18 @@ public class ChaosException extends RuntimeException {
     }
 
     /**
-     * Constructs a new runtime exception with the specified cause and a
-     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
-     * (which typically contains the class and detail message of
-     * <tt>cause</tt>).  This constructor is useful for runtime exceptions
-     * that are little more than wrappers for other throwables.
-     *
      * @param cause the cause (which is saved for later retrieval by the
      *              {@link #getCause()} method).  (A <tt>null</tt> value is
      *              permitted, and indicates that the cause is nonexistent or
      *              unknown.)
      * @since 1.4
+     * @deprecated Constructs a new runtime exception with the specified cause and a
+     * detail message of <tt>(cause==null ? null : cause.toString())</tt>
+     * (which typically contains the class and detail message of
+     * <tt>cause</tt>).  This constructor is useful for runtime exceptions
+     * that are little more than wrappers for other throwables.
      */
+    @Deprecated
     public ChaosException (Throwable cause) {
         this(ChaosErrorCode.GENERIC_FAILURE, cause);
     }
