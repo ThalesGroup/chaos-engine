72bb8c85700d15734670bf5ce60f75108ff2b469
Andrew Mantha <andrew.mantha@gemalto.com>
2019-03-28 10:29:45 -0400

    Remove tests that do not depend on Locale from running with Locale Parameters

--- src/test/java/com/gemalto/chaos/exception/ErrorCodeTest.java
+++ src/test/java/com/gemalto/chaos/exception/ErrorCodeTest.java
@@ -1,8 +1,8 @@
 package com.gemalto.chaos.exception;
 
-import org.hamcrest.CoreMatchers;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.reflections.Reflections;
@@ -10,65 +10,84 @@ import org.reflections.Reflections;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.endsWith;
+import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.*;
 
-@RunWith(Parameterized.class)
+@RunWith(Enclosed.class)
 public class ErrorCodeTest {
-    private final Locale locale;
-    private Collection<Collection<ErrorCode>> allErrorCodes = new HashSet<>();
-
-    public ErrorCodeTest (Locale locale) {
-        this.locale = locale;
-    }
-
-    @Parameterized.Parameters
-    public static Collection<Object[]> getLocales () {
-        final List<Object[]> locales = new ArrayList<>();
-        locales.add(new Locale[]{ Locale.US });
-        for (Locale availableLocale : Locale.getAvailableLocales()) {
-            locales.add(new Locale[]{ availableLocale });
-        }
-        return locales;
-    }
-
-    @Before
-    public void setUp () {
-        Locale.setDefault(locale);
-        allErrorCodes = new Reflections("com.gemalto.chaos").getSubTypesOf(ErrorCode.class)
-                                                            .stream()
-                                                            .map(this::getAllErrorCodes)
-                                                            .peek(errorCodes -> errorCodes.forEach(ErrorCode::clearCachedResourceBundle))
-                                                            .collect(Collectors.toList());
-    }
-
-    private <T extends ErrorCode> Collection<ErrorCode> getAllErrorCodes (Class<T> clazz) {
+    private static <T extends ErrorCode> Collection<ErrorCode> getAllErrorCodes (Class<T> clazz) {
         return Arrays.asList(clazz.getEnumConstants());
     }
 
-    @Test
-    public void noDuplicateErrorCodes () {
-        Set<Integer> usedErrorCodes = new HashSet<>();
-        allErrorCodes.stream()
-                     .flatMap(Collection::stream)
-                     .map(ErrorCode::getErrorCode)
-                     .forEach(integer -> assertTrue("Reused error code " + integer, usedErrorCodes.add(integer)));
+    public static class USLocaleOnly {
+        private Collection<Collection<ErrorCode>> allErrorCodes = new HashSet<>();
+
+        @Before
+        public void setUp () {
+            Locale.setDefault(Locale.US);
+            allErrorCodes = new Reflections("com.gemalto.chaos").getSubTypesOf(ErrorCode.class)
+                                                                .stream()
+                                                                .map(ErrorCodeTest::getAllErrorCodes)
+                                                                .peek(errorCodes -> errorCodes.forEach(ErrorCode::clearCachedResourceBundle))
+                                                                .collect(Collectors.toList());
+        }
+
+        @Test
+        public void noDuplicateErrorCodes () {
+            Set<Integer> usedErrorCodes = new HashSet<>();
+            allErrorCodes.stream()
+                         .flatMap(Collection::stream)
+                         .map(ErrorCode::getErrorCode)
+                         .forEach(integer -> assertTrue("Reused error code " + integer, usedErrorCodes.add(integer)));
+        }
+
+        @Test
+        public void getFormattedMessage () {
+            allErrorCodes.stream()
+                         .flatMap(Collection::stream)
+                         .peek(errorCode -> assertThat(errorCode.getFormattedMessage(), startsWith(String.valueOf(errorCode
+                                 .getErrorCode()))))
+                         .forEach(errorCode -> assertThat(errorCode.getFormattedMessage(), endsWith(errorCode.getLocalizedMessage())));
+        }
     }
 
-    @Test
-    public void containsTranslation () {
-        allErrorCodes.stream().flatMap(Collection::stream).forEach(errorCode -> {
-            final String message = errorCode.getMessage();
-            final String localizedMessage = errorCode.getLocalizedMessage();
-            assertNotEquals(message + " should contain a translation for locale " + locale, message, localizedMessage);
-        });
-    }
+    @RunWith(Parameterized.class)
+    public static class LocaleTests {
+        private final Locale locale;
+        private Collection<Collection<ErrorCode>> allErrorCodes = new HashSet<>();
 
-    @Test
-    public void getFormattedMessage () {
-        allErrorCodes.stream()
-                     .flatMap(Collection::stream)
-                     .peek(errorCode -> assertThat(errorCode.getFormattedMessage(), CoreMatchers.startsWith(String.valueOf(errorCode
-                             .getErrorCode()))))
-                     .forEach(errorCode -> assertThat(errorCode.getFormattedMessage(), CoreMatchers.endsWith(errorCode.getLocalizedMessage())));
+        public LocaleTests (Locale locale) {
+            this.locale = locale;
+        }
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> getLocales () {
+            final List<Object[]> locales = new ArrayList<>();
+            locales.add(new Locale[]{ Locale.US });
+            for (Locale availableLocale : Locale.getAvailableLocales()) {
+                locales.add(new Locale[]{ availableLocale });
+            }
+            return locales;
+        }
+
+        @Before
+        public void setUp () {
+            Locale.setDefault(locale);
+            allErrorCodes = new Reflections("com.gemalto.chaos").getSubTypesOf(ErrorCode.class)
+                                                                .stream()
+                                                                .map(ErrorCodeTest::getAllErrorCodes)
+                                                                .peek(errorCodes -> errorCodes.forEach(ErrorCode::clearCachedResourceBundle))
+                                                                .collect(Collectors.toList());
+        }
+
+        @Test
+        public void containsTranslation () {
+            allErrorCodes.stream().flatMap(Collection::stream).forEach(errorCode -> {
+                final String message = errorCode.getMessage();
+                final String localizedMessage = errorCode.getLocalizedMessage();
+                assertNotEquals(message + " should contain a translation for locale " + locale, message, localizedMessage);
+            });
+        }
     }
 }
