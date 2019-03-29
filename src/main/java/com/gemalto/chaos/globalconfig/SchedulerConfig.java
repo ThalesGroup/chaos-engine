8b1479f9051877be369f68dc02d04e34d0ce4b3a
Andrew Mantha <andrew.mantha@gemalto.com>
2019-03-29 10:09:57 -0400

    SCT-9567 Use a Utility to consolidate the logging into a single method

--- src/main/java/com/gemalto/chaos/globalconfig/SchedulerConfig.java
+++ src/main/java/com/gemalto/chaos/globalconfig/SchedulerConfig.java
@@ -1,14 +1,10 @@
 package com.gemalto.chaos.globalconfig;
 
-import com.gemalto.chaos.ChaosEngine;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.gemalto.chaos.util.ErrorUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
-
-import java.util.Arrays;
 
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
@@ -19,15 +15,7 @@ public class SchedulerConfig implements SchedulingConfigurer {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
         threadPoolTaskScheduler.setThreadNamePrefix("chaos-");
-        threadPoolTaskScheduler.setErrorHandler(ex -> {
-            final Logger logger = Arrays.stream(ex.getStackTrace())
-                                        .map(StackTraceElement::getClassName)
-                                        .filter(s -> s.startsWith("com.gemalto.chaos"))
-                                        .findFirst()
-                                        .map(LoggerFactory::getLogger)
-                                        .orElseGet(() -> LoggerFactory.getLogger(ChaosEngine.class));
-            logger.error("Unhandled Exception in Scheduled Thread", ex);
-        });
+        threadPoolTaskScheduler.setErrorHandler(ErrorUtils::logStacktraceUsingLastValidLogger);
         threadPoolTaskScheduler.initialize();
         scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
     }
