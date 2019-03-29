e2bf5a222ed6d0090ba22a30e1aee7c36561bad1
Andrew Mantha <andrew.mantha@gemalto.com>
2019-03-29 09:05:38 -0400

    Add a global logger to catch Scheduled method exceptions

--- src/main/java/com/gemalto/chaos/globalconfig/SchedulerConfig.java
+++ src/main/java/com/gemalto/chaos/globalconfig/SchedulerConfig.java
@@ -1,9 +1,14 @@
 package com.gemalto.chaos.globalconfig;
 
+import com.gemalto.chaos.ChaosEngine;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.Arrays;
 
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
@@ -14,6 +19,15 @@ public class SchedulerConfig implements SchedulingConfigurer {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
         threadPoolTaskScheduler.setThreadNamePrefix("chaos-");
+        threadPoolTaskScheduler.setErrorHandler(ex -> {
+            final Logger logger = Arrays.stream(ex.getStackTrace())
+                                        .map(StackTraceElement::getClassName)
+                                        .filter(s -> s.startsWith("com.gemalto.chaos"))
+                                        .findFirst()
+                                        .map(LoggerFactory::getLogger)
+                                        .orElseGet(() -> LoggerFactory.getLogger(ChaosEngine.class));
+            logger.error("Unhandled ChaosException in Scheduled Thread", ex);
+        });
         threadPoolTaskScheduler.initialize();
         scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
     }
