3db2c83a3ac1e645def5a346daa7c411a3ca7f62
Lubor PETR <lubor.petr@gemalto.com>
2019-04-16 11:36:54 +0200

    SCT-9585 - Added parametrized capability detection test

--- src/test/java/com/gemalto/chaos/shellclient/ShellClientTest.java
+++ src/test/java/com/gemalto/chaos/shellclient/ShellClientTest.java
@@ -1,27 +1,63 @@
 package com.gemalto.chaos.shellclient;
 
+import com.google.common.primitives.Ints;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.junit.runners.Parameterized;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Parameterized.class)
 public class ShellClientTest {
-    @Mock
     private ShellClient shellClient;
+    private final String capability = randomUUID().toString();
+    private boolean expected;
+    private Map<String, Integer> commandAndExitCode;
+
+    public ShellClientTest (boolean expected, Map<String, Integer> commandAndExitCode) {
+        this.expected = expected;
+        this.commandAndExitCode = commandAndExitCode;
+    }
+
+    @Parameterized.Parameters(name = "Expected {0}, Input combination {1}")
+    public static List<Object[]> params () {
+        int[][] exitCombinations = new int[][]{ { 0, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 }, { 0, 1, 1 }, { 1, 0, 0 }, { 1, 0, 1 }, { 1, 1, 0 }, { 1, 1, 1 }, { -1, -1, -1 }, { 127, 127, 127 }, { 127, 0, 127 } };
+        List<Object[]> parameters = new LinkedList<>();
+        for (int i = 0; i < exitCombinations.length; i++) {
+            boolean expected = false;
+            if (Ints.contains(exitCombinations[i], 0)) {
+                expected = true;
+            }
+            Map<String, Integer> combination = new LinkedHashMap<>();
+            combination.put("command -v ", exitCombinations[i][0]);
+            combination.put("which ", exitCombinations[i][1]);
+            combination.put("type ", exitCombinations[i][2]);
+            parameters.add(new Object[]{ expected, combination });
+        }
+        return parameters;
+    }
+
+    @Before
+    public void setUp () throws Exception {
+        shellClient = mock(ShellClient.class);
+    }
 
     @Test
     public void checkDependency () {
         doCallRealMethod().when(shellClient).checkDependency(any());
-        doReturn(ShellOutput.builder().withExitCode(0).build()).when(shellClient).runCommand("which success");
-        doReturn(ShellOutput.builder().withExitCode(1).build()).when(shellClient).runCommand("which fail");
-        assertTrue(shellClient.checkDependency("success"));
-        assertFalse(shellClient.checkDependency("fail"));
+        for (Map.Entry<String, Integer> entry : commandAndExitCode.entrySet()) {
+            doReturn(ShellOutput.builder().withExitCode(entry.getValue()).build()).when(shellClient)
+                                                                                  .runCommand(entry.getKey() + capability);
+        }
+        assertEquals(expected, shellClient.checkDependency(capability));
     }
 }
