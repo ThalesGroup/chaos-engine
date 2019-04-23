a01c7635ebfa7624da1a0526753e145a35aa5abf
Lubor PETR <lubor.petr@gemalto.com>
2019-04-23 11:02:31 +0200

    SCT-9585 - Dependency test commands moved to ShellConstants

--- src/main/java/com/gemalto/chaos/shellclient/ShellClient.java
+++ src/main/java/com/gemalto/chaos/shellclient/ShellClient.java
@@ -3,17 +3,17 @@ package com.gemalto.chaos.shellclient;
 import org.springframework.core.io.Resource;
 
 import java.io.Closeable;
-import java.util.Arrays;
 
+import static com.gemalto.chaos.shellclient.ShellConstants.DEPENDENCY_TEST_COMMANDS;
 import static com.gemalto.chaos.shellclient.ShellConstants.PARAMETER_DELIMITER;
 
 public interface ShellClient extends Closeable {
     default Boolean checkDependency (String shellCapability) {
-        String[] testCommands = new String[]{ "command -v", "which", "type" };
-        return Arrays.stream(testCommands).map(command -> command + PARAMETER_DELIMITER + shellCapability)
-                     .map(this::runCommand)
-                     .map(ShellOutput::getExitCode)
-                     .anyMatch(exitCode -> exitCode == 0);
+        return DEPENDENCY_TEST_COMMANDS.stream()
+                                       .map(command -> command + PARAMETER_DELIMITER + shellCapability)
+                                       .map(this::runCommand)
+                                       .map(ShellOutput::getExitCode)
+                                       .anyMatch(exitCode -> exitCode == 0);
     }
 
     String runResource (Resource resource);
