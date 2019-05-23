db5a65ab3373b9d87551a0ad85663962487dcd6a
Andrew Mantha <andrew.mantha@gemalto.com>
2019-05-23 14:45:05 -0400

    Add a USA Holiday Calendar module to the Chaos Engine to support US Holidays
    This only supports US Federal holidays. The amount of state-specific holidays varies greatly. This could be expanded upon if needed (i.e., if someone wants to make a Texas module, they can extend this quite easily)

---
+++ chaosengine-schedule/usa/src/main/java/com/thales/chaos/calendar/impl/USA.java
@@ -0,0 +1,95 @@
+package com.thales.chaos.calendar.impl;
+
+import com.thales.chaos.calendar.HolidayCalendar;
+import org.springframework.stereotype.Component;
+
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.TimeZone;
+import java.util.TreeSet;
+
+import static java.util.Calendar.*;
+
+@Component("USA")
+public class USA implements HolidayCalendar {
+    private static final String TZ = "America/New_York";
+    private static final ZoneId TIME_ZONE_ID = ZoneId.of(TZ);
+    private static final int START_OF_DAY = 9;
+    private static final int END_OF_DAY = 17;
+    private final Set<Integer> holidays = new TreeSet<>();
+
+    @Override
+    public boolean isHoliday (Calendar day) {
+        int year = day.get(YEAR);
+        if (!holidays.contains(year)) {
+            renderHolidays(year);
+        }
+        return holidays.contains(day.get(DAY_OF_YEAR));
+    }
+
+    private void renderHolidays (int year) {
+        holidays.clear();
+        holidays.addAll(getStaticHolidays(year));
+        holidays.addAll(getMovingHolidays(year));
+        holidays.addAll(getLinkedDays(holidays, year));
+        holidays.add(year);
+    }
+
+    private Set<Integer> getStaticHolidays (int year) {
+        Set<Integer> staticHolidays = new TreeSet<>();
+        // New Years
+        staticHolidays.add(getDate(year, JANUARY, 1));
+        // Canada Day
+        staticHolidays.add(getDate(year, JULY, 4));
+        // Veterans Day
+        staticHolidays.add(getDate(year, NOVEMBER, 11));
+        // Christmas Eve through New Years
+        staticHolidays.add(getDate(year, DECEMBER, 24));
+        staticHolidays.add(getDate(year, DECEMBER, 25));
+        staticHolidays.add(getDate(year, DECEMBER, 26));
+        staticHolidays.add(getDate(year, DECEMBER, 27));
+        staticHolidays.add(getDate(year, DECEMBER, 28));
+        staticHolidays.add(getDate(year, DECEMBER, 29));
+        staticHolidays.add(getDate(year, DECEMBER, 30));
+        staticHolidays.add(getDate(year, DECEMBER, 31));
+        return staticHolidays;
+    }
+
+    private Set<Integer> getMovingHolidays (int year) {
+        Set<Integer> movingHolidays = new TreeSet<>();
+        // Martin Luther King Day, third Monday of January
+        movingHolidays.add(getDate(year, JANUARY, 3, MONDAY));
+        // Presidents Day, third Monday of February
+        movingHolidays.add(getDate(year, FEBRUARY, 3, MONDAY));
+        // Memorial Day, last Monday in May
+        movingHolidays.add(getDate(year, MAY, -1, MONDAY));
+        // Labour Day, first Monday of September.
+        movingHolidays.add(getDate(year, SEPTEMBER, 1, MONDAY));
+        // Columbus Day, second Monday of October
+        movingHolidays.add(getDate(year, OCTOBER, 2, MONDAY));
+        // Thanksgiving, fourth Thursday of November
+        movingHolidays.add(getDate(year, NOVEMBER, 4, THURSDAY));
+        return movingHolidays;
+    }
+
+    @Override
+    public ZoneId getTimeZoneId () {
+        return TIME_ZONE_ID;
+    }
+
+    @Override
+    public int getStartOfDay () {
+        return START_OF_DAY;
+    }
+
+    @Override
+    public int getEndOfDay () {
+        return END_OF_DAY;
+    }
+
+    @Override
+    public TimeZone getTimeZone () {
+        return TimeZone.getTimeZone(TZ);
+    }
+}
