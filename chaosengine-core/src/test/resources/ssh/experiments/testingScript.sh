434820f2ddfb980fd0ef2cadec5fc91364dbd832
Andrew Mantha <andrew.mantha@gemalto.com>
2019-06-05 10:10:43 -0400

    Remove example scripts from test path so that scripts from main code are passed through
    This ensures we can run validity testing on the scripts in the main path as part of the test suite.

--- chaosengine-core/src/test/resources/ssh/experiments/testingScript.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Dependencies: lots, of, dependencies
-# Description: This is for testing the ScriptManager loader
-# Cattle Only: asdf
-# Health check: echo 'hello, world'
-# Self healing: init 6
-
-echo "Hello, world"
