9256ffe6e11cd9ab867aba1fa9e8f3129eb4f5bf
Andrew Mantha <andrew.mantha@gemalto.com>
2019-06-07 11:37:03 -0400

    Updated the terminateProcess script with 'Physical' and 'Container' appropriate actions
    Using the symlink of /proc/1/exe and checking if it is systemd or init.
    
    The physical branch kills several processes based on matching names. There has been room left to add more processes, and left in such a way that is git-friendly
    
    The container variant just attempts to kill pid 1 with a SIGINT

--- chaosengine-core/src/main/resources/ssh/experiments/terminateProcess.sh
+++ chaosengine-core/src/main/resources/ssh/experiments/terminateProcess.sh
@@ -1,6 +1,23 @@
 #!/bin/sh
 # Description: Terminate random processes
-# Dependencies: kill, ls, grep, sort,  head
+# Dependencies: for, kill, sleep, ps, grep, awk, readlink, set
 
+if readlink -f /proc/1/exe | grep -q -e systemd -e init ; then
+    ps -A -o pid,comm | grep -i \
+      -e docker \
+      -e java \
+      -e python \
+      -e mysql \
+      -e cassandra \
+      -e node \
+      -e etcd \
+      -e mongod \
+      | awk ' { print $1 } ' \
+      | xargs kill -9
 
-kill $(cd /proc;ls -1 | grep '[0-9]' |sort -R | head -1)
+else
+    for _ in 1 2 3 4 5; do
+      kill -2 1
+      sleep 2s
+    done
+fi
