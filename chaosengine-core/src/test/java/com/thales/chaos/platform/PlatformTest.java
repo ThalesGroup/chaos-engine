041c5ec84397858a85627d80ab0b0d0a12f1e82b
Andrew Mantha <andrew.mantha@gemalto.com>
2019-07-03 11:49:37 -0400

    Add a method to the Platform class to get a Collection of Containers matching an AggregationIdentifier

--- chaosengine-core/src/test/java/com/thales/chaos/platform/PlatformTest.java
+++ chaosengine-core/src/test/java/com/thales/chaos/platform/PlatformTest.java
@@ -5,6 +5,7 @@ import com.thales.chaos.experiment.enums.ExperimentType;
 import com.thales.chaos.platform.enums.ApiStatus;
 import com.thales.chaos.platform.enums.PlatformHealth;
 import com.thales.chaos.platform.enums.PlatformLevel;
+import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,6 +18,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -81,5 +83,21 @@ public class PlatformTest {
         platform.getRoster();
         verify(platform, times(2)).generateRoster();
         assertThat(platform.getRoster(), IsIterableContainingInAnyOrder.containsInAnyOrder(container));
+    }
+
+    @Test
+    public void getRosterByAggregationIdentifier () {
+        Container container1 = mock(Container.class);
+        Container container2 = mock(Container.class);
+        Container container3 = mock(Container.class);
+        Container container4 = mock(Container.class);
+        doReturn("aggregateOne").when(container1).getAggregationIdentifier();
+        doReturn("aggregateOne").when(container2).getAggregationIdentifier();
+        doReturn("aggregateTwo").when(container3).getAggregationIdentifier();
+        doReturn("aggregateTwo").when(container4).getAggregationIdentifier();
+        doReturn(List.of(container1, container2, container3, container4)).when(platform).getRoster();
+        assertThat(platform.getRosterByAggregationId("aggregateOne"), containsInAnyOrder(container1, container2));
+        assertThat(platform.getRosterByAggregationId("aggregateTwo"), containsInAnyOrder(container3, container4));
+        assertThat(platform.getRosterByAggregationId("aggregateThree"), IsEmptyCollection.empty());
     }
 }
