558d73fa90dd718ab79505e65c989f3a85f0f1d4
Lubor PETR <lubor.petr@gemalto.com>
2019-07-24 14:16:59 +0200

    SCT-11246 - Added methods to map strings the PFC application route

--- chaosengine-experiments/chaosengine-pcf/src/main/java/com/thales/chaos/container/enums/CloudFoundryApplicationRouteType.java
+++ chaosengine-experiments/chaosengine-pcf/src/main/java/com/thales/chaos/container/enums/CloudFoundryApplicationRouteType.java
@@ -1,6 +1,28 @@
 package com.thales.chaos.container.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum CloudFoundryApplicationRouteType {
-    http,
-    tcp
+    HTTP("http"),
+    TCP("tcp"),
+    UNKNOWN("unknown");
+    private static final Map<String, CloudFoundryApplicationRouteType> invertedStringMap;
+
+    static {
+        invertedStringMap = new HashMap<>();
+        for (CloudFoundryApplicationRouteType value : values()) {
+            invertedStringMap.put(value.rawName, value);
+        }
+    }
+
+    private final String rawName;
+
+    CloudFoundryApplicationRouteType (String rawName) {
+        this.rawName = rawName;
+    }
+
+    public static CloudFoundryApplicationRouteType mapFromString (String string) {
+        return invertedStringMap.getOrDefault(string, UNKNOWN);
+    }
 }
