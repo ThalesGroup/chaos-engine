b45ade2e412e19223bef788198e1599c8b4669fe
Andrew Mantha <andrew.mantha@gemalto.com>
2019-07-24 13:23:01 -0400

    Amend the Identifier class to include Runtime Retention.

--- chaosengine-core/src/main/java/com/thales/chaos/container/annotations/Identifier.java
+++ chaosengine-core/src/main/java/com/thales/chaos/container/annotations/Identifier.java
@@ -1,5 +1,9 @@
 package com.thales.chaos.container.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Identifier {
     int order () default Integer.MAX_VALUE;
 }
