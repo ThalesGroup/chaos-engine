2ca91b87c2b6a9adf7df5055a683ab0c51a667fe
Andrew Mantha <andrew.mantha@gemalto.com>
2019-08-20 10:09:05 -0400

    Minor fixes and logging improvements to the LoggingManager

--- chaosengine-core/src/main/java/com/thales/chaos/logging/LoggingManager.java
+++ chaosengine-core/src/main/java/com/thales/chaos/logging/LoggingManager.java
@@ -3,6 +3,8 @@ package com.thales.chaos.logging;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
@@ -14,8 +16,10 @@ import java.util.concurrent.TimeUnit;
 
 @Component
 public class LoggingManager {
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(LoggingManager.class);
     private static final Logger CHAOS_LOGGER = (Logger) LoggerFactory.getLogger("com.thales.chaos");
     private static final Duration DEFAULT_DEBUG_TIMEOUT = Duration.ofMinutes(30);
+    private static final Marker ALWAYS = MarkerFactory.getMarker("ALWAYS");
     private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
     private ScheduledFuture<?> schedule;
 
@@ -24,20 +28,22 @@ public class LoggingManager {
     }
 
     void setDebugMode (Duration timeout) {
+        logger.info(ALWAYS, "Entering debug mode for {}", timeout);
         CHAOS_LOGGER.setLevel(Level.DEBUG);
-        stopScheduledClear();
+        stopScheduledClear(true);
         schedule = executor.schedule(this::clearDebugMode, timeout.toMillis(), TimeUnit.MILLISECONDS);
     }
 
-    private void stopScheduledClear () {
+    private void stopScheduledClear (boolean mayInterruptIfRunning) {
         if (schedule == null) return;
-        if (!schedule.isDone()) schedule.cancel(true);
+        if (!schedule.isDone()) schedule.cancel(mayInterruptIfRunning);
         schedule = null;
     }
 
     void clearDebugMode () {
+        logger.info(ALWAYS, "Debug mode expired");
         CHAOS_LOGGER.setLevel(null);
-        schedule.cancel(false);
+        stopScheduledClear(false);
     }
 
     @PreDestroy
