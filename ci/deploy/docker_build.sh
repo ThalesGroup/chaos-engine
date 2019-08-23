2524d10b27e9e127db4dcada969c5abad08fe136
Andrew Mantha <andrew.mantha@gemalto.com>
2019-08-23 13:42:27 -0400

    Revert to using direct script commands because the Kaniko container doesn't expose its shell.

--- ci/deploy/docker_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#
-# Build a Docker Image with Kaniko
-
-echo "{\"auths\":{\"$CI_REGISTRY\":{\"username\":\"$CI_REGISTRY_USER\",\"password\":\"$CI_REGISTRY_PASSWORD\"}}}" > /kaniko/.docker/config.json
-/kaniko/executor $KANIKO_OPTS --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG
