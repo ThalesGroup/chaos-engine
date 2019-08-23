113895e65fc3aaa174e5e0582c746bd7477da830
Andrew Mantha <andrew.mantha@gemalto.com>
2019-08-23 12:26:51 -0400

    Move the Docker Build commands into a script

---
+++ ci/deploy/docker_build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Build a Docker Image with Kaniko
+
+echo "{\"auths\":{\"$CI_REGISTRY\":{\"username\":\"$CI_REGISTRY_USER\",\"password\":\"$CI_REGISTRY_PASSWORD\"}}}" > /kaniko/.docker/config.json
+/kaniko/executor --skip-tls-verify --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG
