c745307c43d63c897db11c5aeb64d4dc504506d9
Andrew Mantha <andrew.mantha@gemalto.com>
2019-08-23 12:29:26 -0400

    Add a KANIKO_OPTS variable

--- ci/deploy/docker_build.sh
+++ ci/deploy/docker_build.sh
@@ -3,4 +3,4 @@
 # Build a Docker Image with Kaniko
 
 echo "{\"auths\":{\"$CI_REGISTRY\":{\"username\":\"$CI_REGISTRY_USER\",\"password\":\"$CI_REGISTRY_PASSWORD\"}}}" > /kaniko/.docker/config.json
-/kaniko/executor --skip-tls-verify --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG
+/kaniko/executor $KANIKO_OPTS --context $CI_PROJECT_DIR --dockerfile $CI_PROJECT_DIR/Dockerfile --destination $CI_REGISTRY_IMAGE:$CI_COMMIT_TAG
