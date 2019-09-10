290c46d9260e4269bfeec868de6b5c3cb03309fa
Lubor PETR <lubor.petr@gemalto.com>
2019-09-10 15:34:30 +0200

    SCT-11245 - Black Duck scanner script

---
+++ ci/test/black_duck_scan.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bash <(curl -s -L https://detect.synopsys.com/detect.sh) \
+    --blackduck.url=$BLACK_DUCK_URL \
+    --detect.project.name="$BLACK_DUCK_PROJECT_NAME" \
+    --blackduck.api.token= "$BLACK_DUCK_TOKEN" \
+    --detect.policy.check.fail.on.severities="CRITICAL,BLOCKER" \
+    --detect.report.timeout=600
