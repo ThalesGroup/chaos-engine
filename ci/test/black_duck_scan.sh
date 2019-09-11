7503084dcc8ded493cf996f20c70bcf86eb3b980
Lubor PETR <lubor.petr@gemalto.com>
2019-09-11 13:53:13 +0200

    SCT-11245 - Removed redundant white space in BD token variable

--- ci/test/black_duck_scan.sh
+++ ci/test/black_duck_scan.sh
@@ -3,6 +3,6 @@
 bash <(curl -s -L https://detect.synopsys.com/detect.sh) \
     --blackduck.url=$BLACK_DUCK_URL \
     --detect.project.name="$BLACK_DUCK_PROJECT_NAME" \
-    --blackduck.api.token= "$BLACK_DUCK_TOKEN" \
+    --blackduck.api.token="$BLACK_DUCK_TOKEN" \
     --detect.policy.check.fail.on.severities="CRITICAL,BLOCKER" \
     --detect.report.timeout=600
