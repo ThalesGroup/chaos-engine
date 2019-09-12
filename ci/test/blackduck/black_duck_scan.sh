da1f0833a539e71c3ac0a993543e9b02eda7b06f
Lubor PETR <lubor.petr@gemalto.com>
2019-09-12 14:32:25 +0200

    SCT-11245 - BD severities controlled by global variables

--- ci/test/blackduck/black_duck_scan.sh
+++ ci/test/blackduck/black_duck_scan.sh
@@ -4,5 +4,5 @@ bash <(curl -s -L https://detect.synopsys.com/detect.sh) \
     --blackduck.url=$BLACK_DUCK_URL \
     --detect.project.name="$BLACK_DUCK_PROJECT_NAME" \
     --blackduck.api.token="$BLACK_DUCK_TOKEN" \
-    --detect.policy.check.fail.on.severities="CRITICAL,BLOCKER" \
+    --detect.policy.check.fail.on.severities="$BLACK_DUCK_FAIL_ON_SEVERITIES" \
     --detect.report.timeout=600
