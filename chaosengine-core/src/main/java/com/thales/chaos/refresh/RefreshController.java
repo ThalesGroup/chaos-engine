e944d0cb2efe829d3ddc2a54071c43ee18eaa1c3
Andrew Mantha <andrew.mantha@gemalto.com>
2019-10-03 14:24:24 -0400

    Create a RestController for the Refresh endpoint.

---
+++ chaosengine-core/src/main/java/com/thales/chaos/refresh/RefreshController.java
@@ -0,0 +1,36 @@
+/*
+ *    Copyright (c) 2019 Thales Group
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package com.thales.chaos.refresh;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.endpoint.RefreshEndpoint;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController("/refresh")
+public class RefreshController {
+    @Autowired
+    private RefreshEndpoint refreshEndpoint;
+
+    @PatchMapping
+    public Collection<String> doRefresh () {
+        return refreshEndpoint.refresh();
+    }
+}
