4de8f7def6eee363d8339cb753ba1c4021375be9
Andrew Mantha <andrew.mantha@gemalto.com>
2019-10-03 14:33:58 -0400

    Add a test for the RefreshController class

---
+++ chaosengine-core/src/test/java/com/thales/chaos/refresh/RefreshControllerTest.java
@@ -0,0 +1,59 @@
+/*
+ *    Copyright (c) 2019 Thales Group
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package com.thales.chaos.refresh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.cloud.endpoint.RefreshEndpoint;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebMvcTest(controllers = RefreshController.class)
+public class RefreshControllerTest {
+    @MockBean
+    private RefreshEndpoint refreshEndpoint;
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void doRefresh () throws Exception {
+        doReturn(List.of("passwords", "keys")).when(refreshEndpoint).refresh();
+        mvc.perform(patch("/refresh").contentType(MediaType.APPLICATION_JSON))
+           .andExpect(status().isOk())
+           .andExpect(content().string("[\"passwords\",\"keys\"]"));
+    }
+
+    @Test
+    public void doRefreshFailure () throws Exception {
+        doThrow(new RuntimeException()).when(refreshEndpoint).refresh();
+        mvc.perform(patch("/refresh").contentType(MediaType.APPLICATION_JSON)).andExpect(status().is5xxServerError());
+    }
+}
