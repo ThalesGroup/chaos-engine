61c8ab2fc0475f82d0d2b1842958b01e9dafa771
Andrew Mantha <andrew.mantha@gemalto.com>
2019-10-07 08:53:23 -0400

    Add Swagger ApiOperation

--- chaosengine-core/src/main/java/com/thales/chaos/refresh/RefreshController.java
+++ chaosengine-core/src/main/java/com/thales/chaos/refresh/RefreshController.java
@@ -17,6 +17,7 @@
 
 package com.thales.chaos.refresh;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,6 +31,7 @@ public class RefreshController {
     @Autowired
     private RefreshManager refreshManager;
 
+    @ApiOperation(value = "Trigger Spring Property refresh", notes = "Triggers the Spring Framework backend to refresh all properties loaded from external sources (i.e., vault), and reload any Refreshable Beans that depend on them, such as Platform services")
     @PostMapping
     public Collection<String> doRefresh () {
         return refreshManager.doRefresh();
