fcf301f981394aaeb45ac6aeed468a2a50f67d7f
Andrew Mantha <andrew.mantha@gemalto.com>
2019-10-24 12:32:27 -0400

    Create an implementation of AuthenticationEntryPoint to be used in Security Layer

---
+++ chaosengine-core/src/main/java/com/thales/chaos/security/ChaosAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.thales.chaos.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ChaosAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+}
