0d567bf5ef8fe7659832c6257c77c062ecd211a3
Andrew Mantha <andrew.mantha@gemalto.com>
2019-10-25 09:07:06 -0400

    Create an AuthenticationSuccessHandler to be used in API Security layer

---
+++ chaosengine-core/src/main/java/com/thales/chaos/security/ChaosAuthenticationSuccessHandler.java
@@ -0,0 +1,42 @@
+package com.thales.chaos.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class ChaosAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private RequestCache requestCache;
+
+    @Autowired
+    public ChaosAuthenticationSuccessHandler () {
+        this.requestCache = new HttpSessionRequestCache();
+    }
+
+    ChaosAuthenticationSuccessHandler (HttpSessionRequestCache requestCache) {
+        this.requestCache = requestCache;
+    }
+
+    @Override
+    public void onAuthenticationSuccess (HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) {
+        SavedRequest request = requestCache.getRequest(httpServletRequest, httpServletResponse);
+        if (request == null) {
+            clearAuthenticationAttributes(httpServletRequest);
+            return;
+        }
+        String targetUrlParameter = getTargetUrlParameter();
+        if (isAlwaysUseDefaultTargetUrl() || (targetUrlParameter != null && !targetUrlParameter.isEmpty())) {
+            requestCache.removeRequest(httpServletRequest, httpServletResponse);
+            clearAuthenticationAttributes(httpServletRequest);
+            return;
+        }
+        clearAuthenticationAttributes(httpServletRequest);
+    }
+}
