59b12329de464544a19aee780142229a34cdb470
Andrew Mantha <andrew.mantha@gemalto.com>
2019-11-08 15:42:10 -0500

    Add forwarding rules

---
+++ chaosengine-core/src/main/java/com/thales/chaos/web/ChaosWebConfigurer.java
@@ -0,0 +1,15 @@
+package com.thales.chaos.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ChaosWebConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers (ViewControllerRegistry registry) {
+        registry.addViewController("/help").setViewName("forward:/help/");
+        registry.addViewController("/help/").setViewName("forward:index.html");
+        registry.addViewController("/help/**/{[path:[^\\.]*}").setViewName("forward:index.html");
+    }
+}
