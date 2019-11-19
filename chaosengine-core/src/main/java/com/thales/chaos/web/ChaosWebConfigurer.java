9cf554468da1cde82c70e018ac9abf7a63e2a088
Andrew Mantha <andrew.mantha@gemalto.com>
2019-11-19 11:02:24 -0500

    Add a resource loader for the static help documentation.

--- chaosengine-core/src/main/java/com/thales/chaos/web/ChaosWebConfigurer.java
+++ chaosengine-core/src/main/java/com/thales/chaos/web/ChaosWebConfigurer.java
@@ -1,6 +1,7 @@
 package com.thales.chaos.web;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,5 +12,10 @@ public class ChaosWebConfigurer implements WebMvcConfigurer {
         registry.addViewController("/help").setViewName("redirect:/help/");
         registry.addViewController("/help/").setViewName("forward:index.html");
         registry.addViewController("/help/**/{[path:[^\\.]*}").setViewName("forward:index.html");
+    }
+
+    @Override
+    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/help/**").addResourceLocations("classpath:/static/help/");
     }
 }
