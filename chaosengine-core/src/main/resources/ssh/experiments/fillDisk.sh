a129714eeadd5ba1f53b03ccdd0eed4d0c583e58
Lubor PETR <lubor.petr@thalesgroup.com>
2020-02-04 11:51:46 +0100

    Select blob destination according to tmp filesystem type

--- chaosengine-core/src/main/resources/ssh/experiments/fillDisk.sh
+++ chaosengine-core/src/main/resources/ssh/experiments/fillDisk.sh
@@ -2,14 +2,20 @@
 # Description: Gradualy allocates remaining free space in system root partition
 # Dependencies: dd, df, grep, awk, sleep
 
+TMP_FILESYSTEM=$(df /tmp -T | grep '/$' | awk '{print $2}')
 
+if [ "$TMP_FILESYSTEM" == "overlay" ]; then
+  FILE_PATH=/tmp/blob
+else
+  FILE_PATH=$HOME/blob
+fi
 
-FREE_SPACE=$(($(df -amPk | grep '/$' | awk '{print $4}')*1024))
+FREE_SPACE=$(($(df -amPk | grep '/$' | awk '{print $4}') * 1024))
 
-fallocate -l $(($FREE_SPACE*25/100)) /blob
+fallocate -l $(($FREE_SPACE * 25 / 100)) $FILE_PATH
 sleep 60
-fallocate -l $(($FREE_SPACE*50/100)) /blob
+fallocate -l $(($FREE_SPACE * 50 / 100)) $FILE_PATH
 sleep 60
-fallocate -l $(($FREE_SPACE*75/100)) /blob
+fallocate -l $(($FREE_SPACE * 75 / 100)) $FILE_PATH
 sleep 60
-fallocate -l $FREE_SPACE /blob
+fallocate -l $FREE_SPACE $FILE_PATH
