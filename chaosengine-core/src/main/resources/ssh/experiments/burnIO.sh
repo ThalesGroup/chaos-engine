88e4028edcd6ebe9db750e429e253fcd06ebb54d
Lubor PETR <lubor.petr@thalesgroup.com>
2020-02-04 11:53:42 +0100

    Select blob destination according to tmp filesystem type

--- chaosengine-core/src/main/resources/ssh/experiments/burnIO.sh
+++ chaosengine-core/src/main/resources/ssh/experiments/burnIO.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # Dependencies: dd
 
+if [ "$TMP_FILESYSTEM" == "overlay" ]; then
+  FILE_PATH=/tmp/blob
+else
+  FILE_PATH=$HOME/blob
+fi
 
 while true ; do
-    dd if=/dev/zero of=/tmp/burn bs=1M count=1024
+    dd if=/dev/zero of=$FILE_PATH bs=1M count=1024
 done
