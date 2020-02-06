f7f9d36c91aa0be3d6a26df057ccd746d6340ebf
Lubor PETR <lubor.petr@thalesgroup.com>
2020-02-06 09:40:42 +0100

    Rename output file generated by burnIO.sh

--- chaosengine-core/src/main/resources/ssh/experiments/burnIO.sh
+++ chaosengine-core/src/main/resources/ssh/experiments/burnIO.sh
@@ -2,11 +2,12 @@
 # Dependencies: dd, df, grep, awk
 
 TMP_FILESYSTEM=$(df /tmp -T | grep '/$' | awk '{print $2}')
+FILE_NAME=chaos-burn-io-experiment
 
 if [ "$TMP_FILESYSTEM" != "tmpfs" ]; then
-  FILE_PATH=/tmp/blob
+  FILE_PATH=/tmp/$FILE_NAME
 else
-  FILE_PATH=$HOME/blob
+  FILE_PATH=$HOME/$FILE_NAME
 fi
 
 while true ; do
