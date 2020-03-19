15b648f241b8b1271caaa28083aa2bc47ad0ffde
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-19 09:38:38 +0100

    Extend terminateProcess.sh with other victims

--- chaosengine-core/src/main/resources/ssh/experiments/terminateProcess.sh
+++ chaosengine-core/src/main/resources/ssh/experiments/terminateProcess.sh
@@ -2,22 +2,25 @@
 # Description: Terminate random processes
 # Dependencies: for, kill, sleep, ps, grep, awk, readlink, set
 
-if readlink -f /proc/1/exe | grep -q -e systemd -e init ; then
-    ps -A -o pid,comm | grep -i \
-      -e docker \
-      -e java \
-      -e python \
-      -e mysql \
-      -e cassandra \
-      -e node \
-      -e etcd \
-      -e mongod \
-      | awk ' { print $1 } ' \
-      | xargs kill -9
+if readlink -f /proc/1/exe | grep -q -e systemd -e init; then
+  ps -A -o pid,comm | grep -i \
+    -e docker \
+    -e java \
+    -e python \
+    -e mysql \
+    -e cassandra \
+    -e node \
+    -e etcd \
+    -e mongod \
+    -e nginx \
+    -e httpd \
+    -e postgres |
+    awk ' { print $1 } ' |
+    xargs kill -9
 
 else
-    for _ in 1 2 3 4 5; do
-      kill -2 1
-      sleep 2s
-    done
+  for _ in 1 2 3 4 5; do
+    kill -2 1
+    sleep 2s
+  done
 fi
