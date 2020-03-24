55f48695744cd59f1892d3da390a9d5051368658
Lubor PETR <lubor.petr@thalesgroup.com>
2020-03-24 09:53:51 +0100

    Automatic update of copyright in docs

--- ci/docs/mkdocs_pre_build.sh
+++ ci/docs/mkdocs_pre_build.sh
@@ -7,5 +7,7 @@ set -euxo pipefail
 pip --cache-dir .pip install -r ./ci/docs/mkdocs_requirements.txt
 
 VERSION=$(cat version)
+$YEAR=$(date +%Y)
 
+sed -i "s/<!-- year -->/$YEAR/g" docs/mkdocs.yml
 sed -i "s/<!-- version -->/${VERSION:-Local Build}/g" docs/mkdocs.yml
