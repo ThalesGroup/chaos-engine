2c27faf59cfda21c0c814b3d65c532cfce8a2da5
Lubor PETR <lubor.petr@thalesgroup.com>
2020-04-03 09:39:13 +0200

    GcpSqlPlatform roster POC

--- chaosengine-experiments/chaosengine-gcp-sql/src/main/java/com/thales/chaos/platform/impl/GcpSqlPlatform.java
+++ chaosengine-experiments/chaosengine-gcp-sql/src/main/java/com/thales/chaos/platform/impl/GcpSqlPlatform.java
@@ -23,11 +23,12 @@ import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sqladmin.SQLAdmin;
-import com.google.api.services.sqladmin.model.InstancesListResponse;
+import com.google.api.services.sqladmin.model.DatabaseInstance;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.thales.chaos.container.Container;
 import com.thales.chaos.container.ContainerManager;
+import com.thales.chaos.exception.ChaosException;
 import com.thales.chaos.platform.Platform;
 import com.thales.chaos.platform.enums.ApiStatus;
 import com.thales.chaos.platform.enums.PlatformHealth;
@@ -43,6 +44,7 @@ import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @ConditionalOnProperty("gcp.sql")
 @ConfigurationProperties("gcp.sql")
@@ -56,6 +58,7 @@ public class GcpSqlPlatform extends Platform {
     private GcpCredentialsMetadata gcpCredentialsMetadata;
     private Map<String, String> includeFilter = Collections.emptyMap();
     private Map<String, String> excludeFilter = Collections.emptyMap();
+    private static final String INSTANCE_RUNNING = "RUNNABLE";
 
     public GcpSqlPlatform () {
         log.info("GCP SQL Platform created");
@@ -64,8 +67,8 @@ public class GcpSqlPlatform extends Platform {
     @Override
     public ApiStatus getApiStatus () {
         try {
-            getSQLAdmin().instances().list(gcpCredentialsMetadata.getProjectId()).execute();
-        } catch (RuntimeException | IOException | GeneralSecurityException e) {
+            getInstances();
+        } catch (RuntimeException | IOException e) {
             log.error("Caught error when evaluating API Status of Google Cloud Platform", e);
             return ApiStatus.ERROR;
         }
@@ -80,11 +83,8 @@ public class GcpSqlPlatform extends Platform {
     @Override
     public PlatformHealth getPlatformHealth () {
         try {
-            InstancesListResponse resp = getSQLAdmin().instances()
-                                                      .list(gcpCredentialsMetadata.getProjectId())
-                                                      .execute();
-            return !resp.getItems().isEmpty() ? PlatformHealth.OK : PlatformHealth.DEGRADED;
-        } catch (RuntimeException | IOException | GeneralSecurityException e) {
+            return !getInstances().isEmpty() ? PlatformHealth.OK : PlatformHealth.DEGRADED;
+        } catch (RuntimeException | IOException e) {
             log.error("GCP SQL Platform health check failed", e);
             return PlatformHealth.FAILED;
         }
@@ -93,11 +93,30 @@ public class GcpSqlPlatform extends Platform {
     @Override
     protected List<Container> generateRoster () {
         try {
-            getSQLAdmin();
-        } catch (Exception e) {
+            getInstances().stream().filter(Objects::nonNull).filter(this::isReady).forEach(i -> {
+                System.out.println(i.getName());
+                System.out.println(i.getKind());
+                System.out.println("HA " + hasFailoverReplica(i));
+                System.out.println("Replicas " + hasReadReplicas(i));
+                System.out.println(i.getBackendType());
+                System.out.println("====");
+            });
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return null;
+        return Collections.emptyList();
+    }
+
+    private boolean isReady (DatabaseInstance databaseInstance) {
+        return INSTANCE_RUNNING.equals(databaseInstance.getState());
+    }
+
+    private boolean hasFailoverReplica (DatabaseInstance databaseInstance) {
+        return databaseInstance.getFailoverReplica() != null;
+    }
+
+    private List<String> hasReadReplicas (DatabaseInstance databaseInstance) {
+        return databaseInstance.getReplicaNames();
     }
 
     @Override
@@ -105,11 +124,20 @@ public class GcpSqlPlatform extends Platform {
         return false;
     }
 
-    SQLAdmin getSQLAdmin () throws IOException, GeneralSecurityException {
-        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-        HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(googleCredentials);
-        return new SQLAdmin.Builder(httpTransport, jsonFactory, requestInitializer).setServicePath("sql/v1beta4/")
-                                                                                   .build();
+    List<DatabaseInstance> getInstances () throws IOException {
+        return getSQLAdmin().instances().list(gcpCredentialsMetadata.getProjectId()).execute().getItems();
+    }
+
+    SQLAdmin getSQLAdmin () {
+        HttpTransport httpTransport = null;
+        try {
+            httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+            HttpRequestInitializer requestInitializer = new HttpCredentialsAdapter(googleCredentials);
+            return new SQLAdmin.Builder(httpTransport, jsonFactory, requestInitializer).setServicePath("sql/v1beta4/")
+                                                                                       .build();
+        } catch (IOException | GeneralSecurityException e) {
+            throw new ChaosException("GCP_SQL_GENERIC_ERROR", e);
+        }
     }
 }
